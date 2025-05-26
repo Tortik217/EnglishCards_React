3f1f4ee9564ed872d2b0791aa1745d144a9a19ba
Иван Алексеевич Коледов <ivan.koledov@chipdip.ru>
2025-05-26 15:12:55 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import {React} from 'react'
-import {router} from '@/routes/router'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import router from './routes/router'
 
 import './index.css'
+
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ src/routes/pages/components/EngCardsPanel/EngCardsPanel.jsx
@@ -0,0 +1,13 @@
+// import { NavLink } from "react-router-dom";
+
+function CountryPanel() {
+  return (
+    <div className="wrapper border border-primary rounded-3 p-4">
+      <div className="country d-flex gap-4">
+       <p>Hello</p>
+      </div>
+    </div>
+  );
+}
+
+export default CountryPanel;
