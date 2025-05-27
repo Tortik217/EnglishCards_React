59f907150eea17630858a680d6be6402edd69ce5
Иван Алексеевич Коледов <ivan.koledov@chipdip.ru>
2025-05-27 15:02:37 +0300

    fix

--- src/routes/pages/components/EngCardsPanel/EngCardsPanel.jsx
+++ src/routes/pages/components/EngCardsPanel/EngCardsPanel.jsx
@@ -1,10 +1,39 @@
-// import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 function CountryPanel() {
+
+  const [isknown, setIsKnown] = useState(false);
+
+  function changeKnown(param) {
+    setIsKnown(param)
+  }
+
   return (
     <div className="wrapper border border-primary rounded-3 p-4">
-      <div className="country d-flex gap-4">
-       <p>Hello</p>
+      <div className="Card d-flex flex-column align-items-center gap-4">
+        <div className="word">
+          <h3>Dog</h3>
+        </div>
+        {isknown && (
+          <div className="translateWord">
+            <h3>Собака</h3>
+          </div>
+        )}
+
+        <div className="buttonForm d-flex gap-2">
+          <button
+            className="buttonForm_true btn btn-outline-success"
+            onClick={() => changeKnown(false)}
+          >
+            Know
+          </button>
+          <button
+            className="buttonForm_False btn btn-outline-danger"
+            onClick={() => changeKnown(true)}
+          >
+            Don't know
+          </button>
+        </div>
       </div>
     </div>
   );
