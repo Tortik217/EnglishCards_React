6548cb4e23e5c190a7f96e90375cae9a3c6b4293
Tortik_01 <acer@PodIvanKpc.nonton.local>
2025-05-27 22:22:59 +0300

    do single card

--- src/routes/pages/components/EngCardsPanel/EngCardsPanel.jsx
+++ src/routes/pages/components/EngCardsPanel/EngCardsPanel.jsx
@@ -1,13 +1,25 @@
+import React, { useState } from "react";
 import Card from "../Card/Card";
 import words from "@/data/Words";
 
 function EndCardsPanelPanel() {
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextCard = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex + 1 < words.length ? prevIndex + 1 : 0
+    );
+  };
+
+  const currentWord = words[currentIndex];
+
   return (
-    <div className="wrapper d-flex gap-4 flex-wrap">
-      {words.map((word) => {
-        return <Card key={word.id} word={word.word} translate={word.translate}/>
-      })}
+    <div className="wrapper d-flex flex-column align-items-center gap-4">
+      <Card word={currentWord.word} translate={currentWord.translate} />
+      <button onClick={nextCard} className="btn btn-primary">
+        Next
+      </button>
     </div>
   );
 }
